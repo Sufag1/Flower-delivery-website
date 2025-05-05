422f4cf777c6fe06505bc681035b6eab0d6066a9
Farouq <farouqabubakar99@gmail.com>
2025-05-05 12:20:55 +0100

    flowerModels code added

---
+++ backend/models/flowerModels.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const flowerSchema = new Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+    category: {String, required: true}
+});
+
+module.exports = mongoose.model('flower', flowerSchema);

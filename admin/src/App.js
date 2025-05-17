92e40e7ef92f8087abc6c2061e94578ee05c359d
Farouq <farouqabubakar99@gmail.com>
2025-05-17 21:41:14 +0100

    App.js code added

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Navigate to='/flowers' />}></Route>
+        <Route path='/flowers' element={<Flowers/>}></Route>
+        <Route path='/add-flowers' element={<AddFlowers/>}></Route>
+      </Routes>
+    </Router>
+    
   );
 }
 

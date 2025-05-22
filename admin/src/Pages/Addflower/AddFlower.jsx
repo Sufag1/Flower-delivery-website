3e93b2920c3aefe8c69a0a332e8783b9c8f0cd9f
Farouq <farouqabubakar99@gmail.com>
2025-05-22 23:59:05 +0100

    Completed admin page

--- admin/src/Pages/Addflower/AddFlower.jsx
+++ admin/src/Pages/Addflower/AddFlower.jsx
@@ -1,8 +1,14 @@
+import '../Addflower/AddFlower.css';
 import { useState } from "react";
+import UploadCloud from "../Icons/cloud-upload.svg";
 
 const AddFlowers = () => {
     const [ form, setForm ] = useState({name: "", category: "", price: "", description: ""});
     const [ image, setImage] = useState(null);
+    const [ message, setMessage ] = useState("");
+    const [ messageType, setMessageType ] = useState("");
+    const [preview, setPreview] = useState(null);
+
 
 
     const handleChange = (e) => {
@@ -14,6 +20,14 @@ const AddFlowers = () => {
             [fieldName]: fieldValue
         });
 
+    };
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setImage(file);
+            setPreview(URL.createObjectURL(file));
+        }
     };
 
 
@@ -29,7 +43,44 @@ const AddFlowers = () => {
             formData.append("image", image);
         };
 
-        await fetch("http://localhost:4000/api/flowers", { method: "POST", body: formData,});
+
+        try {
+        const res = await fetch("http://localhost:4000/api/flowers", {
+            method: "POST",
+            body: formData,
+        });
+
+        if (res.ok) {
+            setMessage("🌸 Flower Submitted Successfully!");
+            setMessageType("success");
+
+            setForm({
+            name: "",
+            category: "",
+            price: "",
+            description: ""
+            });
+
+            setImage(null);
+            setPreview(null);
+            document.getElementById("imageInput").value = null; 
+
+        } else {
+            setMessage("❌ Failed to submit flower.");
+            setMessageType("error");
+
+        }
+
+        } catch (err) {
+            console.error("Error submitting flower:", err);
+            setMessage("❌ Something went wrong.");
+            setMessageType("error");
+        }
+
+        setTimeout(() => {
+        setMessage("");
+        setMessageType("");
+        }, 4000);
     };
 
 
@@ -37,25 +88,37 @@ const AddFlowers = () => {
     
     return ( 
         <div className="container">
+            {message && (
+                <div className={`alert ${messageType}`}>
+                    {message}
+                </div>
+            )}
             <form onSubmit={handleSubmit} encType="multipart/form-data">
-                <label>Image</label>
-                <input type="file" onChange={(e) => setImage(e.target.files[0])} accept="image/*" required/>
+                <label htmlFor='imageInput'>Image</label>
+                <label htmlFor="imageInput" className='upload-box'>
+                    {preview ? (
+                        <img src={preview} alt='preview' className='preview' />
+                    ) : (
+                        <img className='cloud' src={UploadCloud} alt="upload-icon" />
+                    )}
+                    </label>
+                <input id='imageInput' className='upload-box' type="file" style={{display: "none"}} onChange={handleImageChange} accept="image/*" required/>
                 <label>Name</label>
-                <input name="name" value={form.name} onChange={handleChange} required/>
+                <input type='text' name="name" value={form.name} onChange={handleChange} required/>
 
                 <div className="row">
                     <div className="category">
                         <label>Category</label>
-                        <input name="category" value={form.category} onChange={handleChange} required/>
+                        <input type='text' name="category" value={form.category} onChange={handleChange} required/>
                     </div>
                     <div className="price">
                         <label>Price</label>
-                        <input name="price" value={form.price} onChange={handleChange} required />
+                        <input type='number' name="price" value={form.price} onChange={handleChange} required />
                     </div>
                 </div>
                 <label>Description</label>
                 <textarea name="description" value={form.description} onChange={handleChange} required></textarea>
-                <button onSubmit={handleSubmit}>SUBMIT</button>
+                <button className='submit-btn' onSubmit={handleSubmit}>SUBMIT</button>
             </form>
         </div>
     );
--- admin/src/Pages/Flower/Flower.jsx
+++ admin/src/Pages/Flower/Flower.jsx
@@ -1,46 +1,67 @@
-import '../Flower/Flower.css';
+import "../Flower/Flower.css";
 import { useEffect, useState } from "react";
 const Flowers = () => {
-    const [ flowers, setFlowers ] = useState([]);
+  const [flowers, setFlowers] = useState([]);
 
+  const fetchFlowers = async () => {
+  try {
+    const res = await fetch("http://localhost:4000/api/flowers");
+    const data = await res.json();
+    console.log("Fetched data from backend:", data);
 
-    const fetchFlowers = async () => {
-        const res = await fetch("http://localhost:4000/api/flowers");
-        const data = await res.json();
-        setFlowers(data);
-    };
-    useEffect(() => {
-        fetchFlowers();
-    },[]);
+    if (data.flowers && Array.isArray(data.flowers)) {
+      setFlowers(data.flowers);
+    } else {
+      setFlowers([]); 
+    }
+  } catch (error) {
+    console.error("Failed to fetch flowers:", error);
+    setFlowers([]); 
+  } 
+};
 
+  useEffect(() => {
+    fetchFlowers();
+  }, []);
 
-    const deleteFlower = async (id) => {
-        await fetch("http://localhost:4000/api/flowers" + id, { method: "DELETE"});
-        fetchFlowers();
-    };
+  const deleteFlower = async (id) => {
+    await fetch(`http://localhost:4000/api/flowers/${id}`, { method: "DELETE" });
+    fetchFlowers();
+  };
 
-
-
-    return ( 
-        <div className="container">
-            <div className="flower-list" key={flowers._id}>
-                {flowers.map((flower) => (
-                    <div className="flower-card">
-                        <div className="flower-img">
-                            <img src={flower.image} alt={flower.name} />
-                            <span className="delete-icon" onClick={() => deleteFlower(flower._id)}>-</span>
-                        </div>
-                        <div className="flower-details">
-                            <p><strong>Name:</strong> {flower.name} </p>
-                            <p><strong>Category:</strong> {flower.category} </p>
-                            <p><strong>Price:</strong> {flower.price} </p>
-                            <p><strong>Description:</strong> {flower.description} </p>
-                        </div>
-                    </div>
-                ))};
+  return (
+    <div className="container">
+      <div className="flower-list">
+        {flowers.map((flower) => (
+          <div className="flower-card" key={flower._id}>
+            <div className="flower-img">
+                <img src={`http://localhost:4000/${flower.image}`} alt={flower.name} />              
+              <span
+                className="delete-icon material-symbols-outlined"
+                onClick={() => deleteFlower(flower._id)}
+              >
+                delete
+              </span>
             </div>
-        </div>
-    );
+            <div className="flower-details">
+              <p>
+                <strong>Name:</strong> {flower.name}
+              </p>
+              <p>
+                <strong>Category:</strong> {flower.category}
+              </p>
+              <p>
+                <strong>Price:</strong> {flower.price}
+              </p>
+              <p>
+                <strong>Description:</strong> {flower.description}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Flowers;

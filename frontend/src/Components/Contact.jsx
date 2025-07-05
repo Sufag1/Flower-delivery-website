4100e41d9b53ba70171e368feca9c964fb98d373
Farouq <farouqabubakar99@gmail.com>
2025-07-05 23:53:42 +0100

    Some retouch done on the home page

--- frontend/src/Components/Contact.jsx
+++ frontend/src/Components/Contact.jsx
@@ -11,89 +11,44 @@ import Tele from '../assets/Telegram.svg';
 const Contact = () => {
     return ( 
         <>
-        <div className="contact">
-            <div className="right-column">
-                <img src={RightColumn} alt="Right"/>
-                <div className="followa">
-                <div className="followh3">
+        <div className="contact-main-div">
+            <div className="kyiv">
+                <div className="kyiv-pic"><img src={RightColumn} alt="Kyiv Pic" /></div>
+                <div className="kyiv-follow">
                     <h3>Follow us</h3>
+                    <div className="follow-ic">
+                        <img src={Insta} alt="Instagram Icon" /><img src={Pinterest} alt="Pinterest Icon" /><img src={Facebook} alt="Facebook Icon" /><img src={Twitter} alt="Twitter Icon" /><img src={Tele} alt="Telegram Icon" />
+                    </div>
                 </div>
-                <div className="follow-vl"></div>
-                <div className="follow-ic">
-                    <img src={Insta} alt="instagram icon" /><img src={Pinterest} alt="pinterest icon" /><img src={Facebook} alt="facebook icon" /><img src={Twitter} alt="twitter icon" /><img src={Tele} alt="telegram icon" />
-                </div>
-                <div className="follow-vl"></div>
-            </div>               
             </div>
-            
-            <div className="left-column">
+
+
+            <div className="to-contact-us">
                 <div className="top-block">
                     <h2>To Contact Us</h2>
-                    <div className="form-block">
-                        <form>
-                            <label>We will call you back</label><br />
-                            <div className="contact-block">
-                                <input type="text" placeholder='+380 XX XXX XX XX'/>
-                                <button>BLOCK A CALL</button>
-                            </div>
-                        </form>
+                    <label>We will call you back</label>
+                    <div className="block-call-div">
+                        <input type="text" placeholder='+380 XX XXX XX XX'/>
+                        <button>BLOCK A CALL</button>
                     </div>
                 </div>
-
+                
                 <div className="bottom-block">
-                    <div className="column1-wrapper">
-                        <div className="column1">
-                            <h3>Phone</h3>
-                        </div>
-                        <div className="column1-vl"></div>
-                        <div className="phone-no">
-                            <div className="phone"><img src={Phone} alt="phone icon" /> <p>+380980099777</p></div>
-                            <div className="phone"><img src={Phone} alt="phone icon" /> <p>+380980099111</p></div>
-                        </div>
-                    </div> 
-                    <div className="column2">
-                        <div className="add">
-                            <h3>Address</h3>
-                        </div>
-                        <div className="location">
-                            <p>Opening hours: 8 to 11 p.m.</p>
-                            <div className="loc">
-                                <img src={Location} alt="location icon" /><p>15/4 Khreshchatyk Street, Kyiv </p>
-                            </div>
+                    <div className="phone-div"><h3>Phone</h3></div>
+                    <div className="phone-no">
+                        <div className="phone-no1"><img src={Phone} alt="Phone Icon" /><h5>+380980099777</h5></div>
+                        <div className="phone-no1"><img src={Phone} alt="Phone Icon" /><h5>+380980099111</h5></div>
+                    </div>
+
+                    <div className="address-div"><h3>Address</h3></div>
+                    <div className="address-loc">
+                        <h5>Opening hours: 8 to 11 p.m.</h5>
+                        <div className="loc">
+                            <img src={Location} alt="Location Icon" /><h6>15/4 Khreshchatyk Street, Kyiv </h6>
                         </div>
                     </div>
                 </div>
-
-                {/* Bottom block 1440px */}
-
-                <div className="bottom-block-1440">
-                    <div className="phone-address-block">
-                        <div className="phone-d">
-                            <h3>Phone</h3>
-                        </div>
-                        <div className="address-d">
-                            <h3>Address</h3>
-                        </div>
-                    </div>
-
-                    <div className="phoneno-address-block">
-                        <div className="phone-no">
-                            <div className="phone"><img src={Phone} alt="phone icon" /> <p>+380980099777</p></div>
-                            <div className="phone"><img src={Phone} alt="phone icon" /> <p>+380980099111</p></div>
-                        </div>
-
-                        <div className="address-no">
-                            <p>Opening hours: 8 to 11 p.m.</p>
-                            <div className="loc">
-                                <img src={Location} alt="location icon" /><p>15/4 Khreshchatyk Street, Kyiv </p>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
             </div>
-
         </div>
         </>
      );
